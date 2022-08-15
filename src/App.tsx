e2731c22b228f5e99eab6aec0761151ac2767a92
Fernando <fernando.bbosco@gmail.com>
2022-08-14 23:02:49 -0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import { Center } from "@chakra-ui/react";
-import AppButton from "./components/Button";
+import {
+  Box,
+  Button,
+  Center,
+  FormLabel, Textarea,
+  useClipboard,
+  VStack
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function App() {
+  const [json, setJson] = useState("");
+  const [csv, setCsv] = useState("");
+  const { hasCopied, onCopy } = useClipboard(csv);
+  function generateCSV(jsonString: string) {
+    const headers = Object.keys(JSON.parse(jsonString));
+    const values = Object.values(JSON.parse(jsonString));
+
+    setCsv(headers.join(",") + " \n\n " + values.join(","));
+  }
   return (
     <Center h="100vh">
-      <AppButton>Do Something!</AppButton>
+      <VStack spacing={2}>
+        <FormLabel>JSON</FormLabel>
+        <Textarea value={json} onChange={(e: any) => setJson(e.target.value)} />
+        <Button onClick={() => generateCSV(json)}>Convert!</Button>
+
+        <Box bg="gray.100" p={2} rounded="lg" w="300px">
+          {csv}
+        </Box>
+
+        <Button onClick={onCopy} ml={2}>
+          {hasCopied ? "Copied" : "Copy"}
+        </Button>
+      </VStack>
     </Center>
   );
 }
